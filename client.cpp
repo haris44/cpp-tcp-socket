8b69450a6aa85f6c968121e860feea1ebb4ab055
nathan <grimaudathan@gmail.com>
2018-04-02 17:22:54 +0200

    feat(socket -> thread)

--- client.cpp
+++ client.cpp
@@ -3,30 +3,77 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <thread>
+#include <vector>
 
 using namespace std;
 
-int main (int argc, char* argv[]) {
-  if(argc != 3) {
+const int num_threads = 10;
+
+void sendTo(ClientSocket &client_socket)
+{
+  string message;
+  try
+  {
+    while (true)
+    {
+      cout << "Send : ";
+      getline(std::cin, message);
+      client_socket << message;
+    }
+  }
+  catch (SocketException &)
+  {
+  }
+}
+
+void listenReply(ClientSocket &client_socket)
+{
+  string reply;
+  try
+  {
+    while (true)
+    {
+      client_socket >> reply;
+      cout << "We received this response from the server:\n\"" << reply << "\"\n";
+    }
+  }
+  catch (SocketException &)
+  {
+  }
+}
+
+int main(int argc, char *argv[])
+{
+
+  string host = argv[1];
+  int port = atoi(argv[2]);
+  if (argc != 3)
+  {
     cout << "invalid number of arguments: call with `./client IP port`" << endl;
     return 1;
   }
 
-  string host = argv[1];
-  int port = atoi(argv[2]);
+  string name;
+  vector<thread> v(thread::hardware_concurrency());
 
-  try {
-    ClientSocket client_socket ( host, port );
-    string reply;
+  try
+  {
+    cout << "name ? : ";
+    getline(std::cin, name);
 
-    try {
-      client_socket << "Test message.";
-      client_socket >> reply;
-    } catch ( SocketException& ) {}
+    ClientSocket client_socket(host, port);
 
-    cout << "We received this response from the server:\n\"" << reply << "\"\n";;
+    client_socket << name;
 
-  }  catch ( SocketException& e ) {
+    thread t1 = thread(listenReply, ref(client_socket));
+    thread t2 = thread(sendTo, ref(client_socket));
+
+    t1.join();
+    t2.join();
+  }
+  catch (SocketException &e)
+  {
     cout << "Exception was caught:" << e.description() << endl;
   }
 
--- server.cpp
+++ server.cpp
@@ -2,38 +2,99 @@
 #include "SocketException.h"
 #include <string>
 #include <iostream>
+#include <thread>
+#include <mutex>
 #include <cstdlib>
-
+#include <vector>
+#include <queue>
+#include <chrono>
+#include <condition_variable>
 using namespace std;
 
-int main (int argc, char* argv[]) {
-  if(argc !=2) {
+class User
+{
+
+public:
+  string name = "0";
+  ServerSocket *socket;
+
+  void say(string message)
+  {
+    *this->socket << message;
+  }
+
+  void broadcast(vector<User *> users, int index, string message)
+  {
+    for (int i = 0; i < users.size(); i++)
+    {
+      if (i != index)
+      {
+        users[i]->say(message);
+      }
+    }
+  }
+};
+
+static vector<thread> pool;
+static vector<User *> users;
+
+void clients_thread(User *user, int index)
+{
+  try
+  {
+    while (true)
+    {
+      string data;
+      *user->socket >> data;
+
+      if (user->name != "0")
+      {
+        string message;
+        message = user->name + " told us : " + data + " !";
+        user->broadcast(users, index, message);
+        *user->socket << "Message sent !";
+      }
+      else
+      {
+        cout << data << " have joined ! " << endl;
+        user->name = data;
+        *user->socket << "Successfully joined !";
+      }
+    }
+  }
+  catch (SocketException &e)
+  {
+    cout << "Exception was caught:" << e.description() << "\nExiting" << endl;
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
     cout << "invalid number of arguments: call with `./server port`" << endl;
     return 1;
   }
 
   int port = atoi(argv[1]);
-
   cout << "running....\n";
-
-  try {
-    ServerSocket server ( port );
-
-    while ( true ) {
-
-      ServerSocket new_sock;
-      server.accept ( new_sock );
-
-      try {
-        while ( true ) {
-          string data;
-          new_sock >> data;
-          new_sock << data;
-        }
-      } catch ( SocketException& ) {}
-
+  try
+  {
+    ServerSocket server(port);
+    while (true)
+    {
+      ServerSocket *new_sock = new ServerSocket();
+      server.accept(*new_sock);
+      User *user = new User();
+      users.push_back(user);
+      user->socket = new_sock;
+      pool.push_back(thread(clients_thread, user, users.size() - 1));
+      std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     }
-  } catch ( SocketException& e ) {
+  }
+
+  catch (SocketException &e)
+  {
     cout << "Exception was caught:" << e.description() << "\nExiting" << endl;
   }
 
